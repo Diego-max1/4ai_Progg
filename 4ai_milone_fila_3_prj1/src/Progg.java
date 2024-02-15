54971e2eb5037310b47d3633afd3f08fadf37811
4a <4a@labinf1-06>
2024-02-15 09:47:40 +0100

    vers 1.0

---
+++ 4ai_milone_fila_3_prj1/src/Progg.java
@@ -0,0 +1,11 @@
+
+public class Progg {
+
+	public static void main(String[] args) {
+		System.out.println("Milone Diego");
+		System.out.println("Peschiulli Leonardo");
+		System.out.println("Maggiore Enrico");
+		System.out.println("Riso Lorenzo");
+	}
+
+}
